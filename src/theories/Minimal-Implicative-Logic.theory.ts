c58332555145c0e4baecf1690e49df5b50de531c
Steven Obua <steven@recursivemind.com>
2025-03-02 14:34:41 +0000

    Added Minimal Logic.

--- src/theories/Minimal-Implicative-Logic.theory.ts
+++ src/theories/Minimal-Implicative-Logic.theory.ts
@@ -1,8 +1,8 @@
-import { beginTheory, declare, axiom, endTheory } from "../workbench.js";
+import { beginTheory, declare, axiom, endTheory, define } from "../workbench.js";
 
 beginTheory();
 declare("implies A B")
-axiom("Modus-ponens", "B", ["implies A B", "A"])
-axiom("Implication_1", "implies A (implies B A)")
-axiom("Implication_2", "implies (implies A (implies B C)) (implies (implies A B) (implies B C))")
+axiom("modus-ponens", "B", ["implies A B", "A"])
+axiom("implies_1", "implies A (implies B A)")
+axiom("implies_2", "implies (implies A (implies B C)) (implies (implies A B) (implies B C))")
 endTheory("Minimal-Implicative-Logic");
---
+++ src/theories/Minimal-Logic.theory.ts
@@ -0,0 +1,14 @@
+import { axiom, beginTheory, declare, endTheory, importTheory } from "../workbench.js";
+import "./Minimal-Implicative-Logic.theory.js";
+
+beginTheory();
+importTheory("Minimal-Implicative-Logic");
+declare("and A B");
+declare("or A B");
+axiom("or-intro_1", "implies A (or A B)");
+axiom("or-intro_2", "implies B (or A B)");
+axiom("or-elim", "implies (implies A C) (implies (implies B C) (implies (or A B) C))"); 
+axiom("and-elim_1", "implies (and A B) A");
+axiom("and-elim_2", "implies (and A B) B");
+axiom("and-intro", "implies A (implies B (and A B))");
+endTheory("Minimal-Logic");
