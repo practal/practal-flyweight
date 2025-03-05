4f1aafcb498db9d41735161f48815e1215b17b07
Steven Obua <steven@recursivemind.com>
2025-03-05 16:54:48 +0000

    Universal

--- src/theories/Equality.theory.ts
+++ src/theories/Equality.theory.ts
@@ -1,7 +1,23 @@
-import { beginTheory, declare, axiom, endTheory } from "../workbench.js";
+import { Term } from "../kernel/default-terms.js";
+import { beginTheory, declare, axiom, endTheory, conclOf, infer, print, S, subst, terms, Thm, thm } from "../workbench.js";
+import { modusPonens } from "./Implication.theory.js";
 
 beginTheory();
 declare("equals x y");
 axiom("equals_refl", "equals x x");
 axiom("equals_subst", "A[y]", ["equals x y", "A[x]"]);
 endTheory("Equality");
+
+export function destEquals(implication : Term) : [Term, Term] {
+    const absapp = terms().destAbsApp(implication);
+    if (absapp.length !== 1) throw new Error("destEquals");
+    const [id, args] = absapp[0];
+    if (id !== "equals" || args.length !== 2) throw new Error("destEquals");
+    return [args[0], args[1]];
+}
+
+
+export function substEquals(eq : Thm) : Thm {
+    const [x, y] = destEquals(conclOf(eq));
+    return infer(subst(S("x", x, "y", y), thm("equals_subst")), eq);
+}
--- src/theories/Universal.theory.ts
+++ src/theories/Universal.theory.ts
@@ -1,17 +1,29 @@
-import { beginTheory, importTheory, declare, axiom, define, endTheory } from "../workbench.js";
+import { Term } from "../kernel/default-terms.js";
+import { beginTheory, importTheory, declare, axiom, define, endTheory, note, assume, Thm, thm, S, subst, conclOf, terms, infer, print } from "../workbench.js";
 import "./Equality.theory.js";
+import { substEquals } from "./Equality.theory.js";
 import "./Implication.theory.js";
+import { modusPonens } from "./Implication.theory.js";
+import "./Negation.theory.js";
 
 beginTheory();
 importTheory("Implication");
 importTheory("Equality");
-declare("forall (x. A[x])");
-axiom("Universal_1", "forall (x. A[x])", "x. A[x]");
-axiom("Universal_2", "forall (x. A[x])", ["B", "forall (x. implies B A[x])"]);
-define("false", "false", "forall (x. x)");
+declare("for-all (x. A[x])");
+axiom("for-all_intro", "for-all (x. A[x])", "x. A[x]");
+axiom("for-all_elim", "implies (for-all (x. A[x])) A[x]");
+axiom("for-all_distr", "implies (for-all (x. implies A B[x])) (implies A (for-all (x. B[x])))");
+define("false-def", "false", "for-all (x. x)");
+note("ex-falso-quodlibet", proveExFalsoQuodLibet());
+importTheory("Negation");
 endTheory("Universal");
 
 beginTheory();
 importTheory("Universal");
-axiom("Universal_Ext", "equals (forall (x. A[x])) (forall (x. B[x]))", "x. equals A[x] B[x]");
+axiom("for-all_ext", "equals (for-all (x. A[x])) (for-all (x. B[x]))", "x. equals A[x] B[x]");
 endTheory("Universal_Ext");
+
+function proveExFalsoQuodLibet() : Thm {
+    const elim = subst(S("A", "x. x", "x", "P"), thm("for-all_elim"));
+    return modusPonens(elim, subst(S("A", "x. x"), substEquals(thm("false-def"))));
+}
