31778b3b576caeea14747cb85afd4ccfed2393b1
Steven Obua <steven@recursivemind.com>
2025-03-08 11:27:01 +0000

    Update Lambda-Calculus.theory.ts

--- src/theories/Lambda-Calculus.theory.ts
+++ src/theories/Lambda-Calculus.theory.ts
@@ -1,29 +1,17 @@
-import { assume, axiom, beginTheory, declare, define, endTheory, includeTheory, infer, note, S, subst, thm, Thm } from "../workbench.js";
-import "./Equality.theory.js";
-import { equalsRefl, equalsSym, substEquals } from "./Equality.theory.js";
+import { axiom, beginTheory, declare, define, endTheory, includeTheory } from "../workbench.js";
+import "./Intuitionistic-Predicate-Logic.theory.js";
 
 beginTheory();
-includeTheory("Equality");
+includeTheory("Intuitionistic-Predicate-Logic");
+declare("Lam f");
 declare("lam (x. M[x])");
 declare("app M N");
-axiom("beta", "equals (app (lam (x. M[x])) N) M[N]");
 
-// This should actually lead to inconsistency, 
-// as we are saying that each unary operator corresponds to a lambda object.
-// Would be good to actually prove that here!
+define("Lam (x. M[x])", "for-all (x. implies (Lam x) (Lam M[x]))");
 
-// Let's try to build up logic like Peter Andrews does for HOL, and see if
-// we can invoke Curry's Paradox somehow.
-
-define("true", "equals (lam (x. x)) (lam (x. x))");
-define("for-all (x. A[x])", "equals (lam (x. A[x])) (lam (x. true))")
-define("false", "for-all (x. x)");
-
-note("true", proveTrue());
-
-function proveTrue() : Thm {
-    const refl = equalsRefl("lam (x. x)");
-    return infer(subst(S("A", "x. x"), substEquals(equalsSym(thm("true_def")))), refl);
-}
+axiom("Lam-wf-lam", "Lam (lam (x. M[x]))", "Lam (x. M[x])");
+axiom("Lam-wf-app", "Lam (app M N)", ["Lam M", "Lam N"]);
+axiom("Lam-beta", "equals (app (lam (x. M[x])) N) M[N]", ["Lam (x. M[x])", "Lam N"]);
+axiom("Lam-ext", "equals f g", ["Lam f", "Lam g", "x. implies (Lam x) (equals (app f x) (app g x))"])
 
 endTheory("Lambda-Calculus");
