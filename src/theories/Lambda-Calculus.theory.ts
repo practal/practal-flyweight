2a5db5b6ce7228500ede1a3f12b126fc66df5422
Steven Obua <steven@recursivemind.com>
2025-03-09 15:52:13 +0000

    Update Lambda-Calculus.theory.ts

--- src/theories/Lambda-Calculus.theory.ts
+++ src/theories/Lambda-Calculus.theory.ts
@@ -1,17 +1,26 @@
 import { axiom, beginTheory, declare, define, endTheory, includeTheory } from "../workbench.js";
-import "./Intuitionistic-Predicate-Logic.theory.js";
+import "./Implication.theory.js";
+import "./Equality.theory.js";
 
 beginTheory();
-includeTheory("Intuitionistic-Predicate-Logic");
+includeTheory("Equality");
+includeTheory("Implication");
 declare("Lam f");
 declare("lam (x. M[x])");
 declare("app M N");
 
-define("Lam (x. M[x])", "for-all (x. implies (Lam x) (Lam M[x]))");
+declare("Lam (x. M[x])");
+axiom("Lam", "Lam (x. M[x])", "x. implies (Lam x) (Lam M[x])");
 
 axiom("Lam-wf-lam", "Lam (lam (x. M[x]))", "Lam (x. M[x])");
 axiom("Lam-wf-app", "Lam (app M N)", ["Lam M", "Lam N"]);
 axiom("Lam-beta", "equals (app (lam (x. M[x])) N) M[N]", ["Lam (x. M[x])", "Lam N"]);
-axiom("Lam-ext", "equals f g", ["Lam f", "Lam g", "x. implies (Lam x) (equals (app f x) (app g x))"])
 
 endTheory("Lambda-Calculus");
+
+beginTheory();
+includeTheory("Lambda-Calculus");
+
+axiom("Lam-ext", "equals f g", ["Lam f", "Lam g", "x. implies (Lam x) (equals (app f x) (app g x))"])
+
+endTheory("Lambda-Calculus-Ext");
